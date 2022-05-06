92b7e5f5cf34da04f8e182c92fe2968c19990edb
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-05-06 17:31:59 -0500

    Added toggle for Edit/Del note buttons

--- src/components/NoteGroup.js
+++ src/components/NoteGroup.js
@@ -21,6 +21,7 @@ const NoteGroup = () => {
     const [confirmDelete, setDeleteConfirmation] = useState(false);
     const [editFlag, setEditFlag] = useState(false);
     const [editNoteId, setEditNoteId] = useState("");
+    const [displayNoteButtons, setDisplayNoteButtons] = useState(false);
     
     const handleNewNoteClick = () => {
         setShowForm(true);
@@ -68,8 +69,10 @@ const NoteGroup = () => {
         return (
             <li key={note._id} style={listStyle}>
                 <div style={noteLineStyle}>
-                    <EditNoteButton style={noteBtnStyle} noteId={note._id} setEditFlag={setEditFlag} setEditNoteId={setEditNoteId}/>
-                    <DeleteNoteButton style={noteBtnStyle} noteId={note._id} setDeleteFlag={setDeleteFlag} setDeleteNoteId={setDeleteNoteId}/>
+                    {displayNoteButtons ? <EditNoteButton style={noteBtnStyle} noteId={note._id} setEditFlag={setEditFlag} setEditNoteId={setEditNoteId}/> : ""}
+                    
+                    {displayNoteButtons ? <DeleteNoteButton style={noteBtnStyle} noteId={note._id} setDeleteFlag={setDeleteFlag} setDeleteNoteId={setDeleteNoteId}/> : ""}
+                    
                     <Note 
                         content={note.content} 
                         noteId={note._id}
@@ -98,13 +101,36 @@ const NoteGroup = () => {
         paddingLeft: "0px"
     }
 
+    const checkboxStyle = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginLeft: "5rem"
+    }
+
+    const notesTitleStyle = {
+        marginBottom: "0px"
+    }
+
+    const boxStyle = {
+        marginRight: "0.5rem"
+    }
+
+    const displayCheckboxFlag = () => {
+        setDisplayNoteButtons(!displayNoteButtons);
+    }
+
     return (
         <div style={noteGroupStyle}>
             <div style={noteTitleStyle}>
-                <h2>Notes</h2>
+                <h2 style={notesTitleStyle}>Notes</h2>
                 <Button style={noteBtnStyle} variant="success" onClick={handleNewNoteClick}>
                     +
                 </Button>
+                <div style={checkboxStyle}>
+                    <input style={boxStyle} id="editNotes" onChange={displayCheckboxFlag} type="checkbox" />
+                    <label htmlFor="editNotes">Edit Notes</label>
+                </div>
             </div>
             {showForm ? <NoteForm content={content} setContent={setContent} setShowForm={setShowForm}/> : ""}
             <ul style={listGroupStyle}>
