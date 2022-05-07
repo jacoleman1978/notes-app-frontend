d0cfce0102df5eccae65830bb2a1860a48b01f47
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-05-07 09:58:31 -0500

    Changed axios addresses for Netlify

--- src/services/noteDataService.js
+++ src/services/noteDataService.js
@@ -4,43 +4,84 @@ import axios from 'axios';
 class NoteDataService {
     static NewTopic(userName, parentTopicId, topicName) {
         axios.defaults.withCredentials = true;
-        return axios.post(`http://localhost:4343/notes/${userName}/${parentTopicId}/new/topic`, {topicName: topicName})
+        return axios.post(`https://notes-app-milestone3.herokuapp.com/notes/${userName}/${parentTopicId}/new/topic`, {topicName: topicName})
     }
 
     static GetHomeDirectory(userName) {
         axios.defaults.withCredentials = true;
-        return axios.get(`http://localhost:4343/notes/${userName}`)
+        return axios.get(`https://notes-app-milestone3.herokuapp.com/notes/${userName}`)
     }
 
     static GetTopicsAndNotes(userName, topicId) {
         axios.defaults.withCredentials = true;
-        return axios.get(`http://localhost:4343/notes/${userName}/${topicId}`)
+        return axios.get(`https://notes-app-milestone3.herokuapp.com/notes/${userName}/${topicId}`)
     }
 
     static EditTopic(userName, topicId, topicName) {
         axios.defaults.withCredentials = true;
-        return axios.put(`http://localhost:4343/notes/${userName}/${topicId}`, {topicName: topicName})
+        return axios.put(`https://notes-app-milestone3.herokuapp.com/notes/${userName}/${topicId}`, {topicName: topicName})
     }
 
     static DeleteTopic(userName, topicId) {
         axios.defaults.withCredentials = true;
-        return axios.delete(`http://localhost:4343/notes/${userName}/${topicId}`)
+        return axios.delete(`https://notes-app-milestone3.herokuapp.com/notes/${userName}/${topicId}`)
     }
 
     static NewNote(userName, parentTopicId, content) {
         axios.defaults.withCredentials = true;
-        return axios.post(`http://localhost:4343/notes/${userName}/${parentTopicId}/new/note`, {content: content})
+        return axios.post(`https://notes-app-milestone3.herokuapp.com/notes/${userName}/${parentTopicId}/new/note`, {content: content})
     }
 
     static EditNote(userName, parentTopicId, noteId, content) {
         axios.defaults.withCredentials = true;
-        return axios.put(`http://localhost:4343/notes/${userName}/${parentTopicId}/${noteId}`, {content: content})
+        return axios.put(`https://notes-app-milestone3.herokuapp.com/notes/${userName}/${parentTopicId}/${noteId}`, {content: content})
     }
 
     static DeleteNote(userName, parentTopicId, noteId) {
         axios.defaults.withCredentials = true;
-        return axios.delete(`http://localhost:4343/notes/${userName}/${parentTopicId}/${noteId}`)
+        return axios.delete(`https://notes-app-milestone3.herokuapp.com/notes/${userName}/${parentTopicId}/${noteId}`)
     }
 }
+
+//     static NewTopic(userName, parentTopicId, topicName) {
+//         axios.defaults.withCredentials = true;
+//         return axios.post(`http://localhost:4343/notes/${userName}/${parentTopicId}/new/topic`, {topicName: topicName})
+//     }
+
+//     static GetHomeDirectory(userName) {
+//         axios.defaults.withCredentials = true;
+//         return axios.get(`http://localhost:4343/notes/${userName}`)
+//     }
+
+//     static GetTopicsAndNotes(userName, topicId) {
+//         axios.defaults.withCredentials = true;
+//         return axios.get(`http://localhost:4343/notes/${userName}/${topicId}`)
+//     }
+
+//     static EditTopic(userName, topicId, topicName) {
+//         axios.defaults.withCredentials = true;
+//         return axios.put(`http://localhost:4343/notes/${userName}/${topicId}`, {topicName: topicName})
+//     }
+
+//     static DeleteTopic(userName, topicId) {
+//         axios.defaults.withCredentials = true;
+//         return axios.delete(`http://localhost:4343/notes/${userName}/${topicId}`)
+//     }
+
+//     static NewNote(userName, parentTopicId, content) {
+//         axios.defaults.withCredentials = true;
+//         return axios.post(`http://localhost:4343/notes/${userName}/${parentTopicId}/new/note`, {content: content})
+//     }
+
+//     static EditNote(userName, parentTopicId, noteId, content) {
+//         axios.defaults.withCredentials = true;
+//         return axios.put(`http://localhost:4343/notes/${userName}/${parentTopicId}/${noteId}`, {content: content})
+//     }
+
+//     static DeleteNote(userName, parentTopicId, noteId) {
+//         axios.defaults.withCredentials = true;
+//         return axios.delete(`http://localhost:4343/notes/${userName}/${parentTopicId}/${noteId}`)
+//     }
+// }
 
 export default NoteDataService;
--- src/services/userDataService.js
+++ src/services/userDataService.js
@@ -4,26 +4,49 @@ import axios from 'axios';
 class UserDataService {
     static Login(data) {  
         axios.defaults.withCredentials = true
-        return axios.post('http://localhost:4343/auth/login', data)
+        return axios.post('https://notes-app-milestone3.herokuapp.com/auth/login', data)
     }
 
     static Signup(data) {
         axios.defaults.withCredentials = true
-        return axios.post('http://localhost:4343/auth/signup', data)
+        return axios.post('https://notes-app-milestone3.herokuapp.com/auth/signup', data)
     }
 
     static IsSignupInfoUnique(data) {
         axios.defaults.withCredentials = true
-        return axios.post('http://localhost:4343/auth/signup/verify', data)
+        return axios.post('https://notes-app-milestone3.herokuapp.com/auth/signup/verify', data)
     }
 
     static CheckSessionUser() {
-        return axios.get('http://localhost:4343/auth/session', {withCredentials: true})
+        return axios.get('https://notes-app-milestone3.herokuapp.com/auth/session', {withCredentials: true})
     }
 
     static Logout() {
-        return axios.get('http://localhost:4343/auth/logout', {withCredentials: true})
+        return axios.get('https://notes-app-milestone3.herokuapp.com/auth/logout', {withCredentials: true})
     }    
+    
+    // static Login(data) {  
+    //     axios.defaults.withCredentials = true
+    //     return axios.post('http://localhost:4343/auth/login', data)
+    // }
+
+    // static Signup(data) {
+    //     axios.defaults.withCredentials = true
+    //     return axios.post('http://localhost:4343/auth/signup', data)
+    // }
+
+    // static IsSignupInfoUnique(data) {
+    //     axios.defaults.withCredentials = true
+    //     return axios.post('http://localhost:4343/auth/signup/verify', data)
+    // }
+
+    // static CheckSessionUser() {
+    //     return axios.get('http://localhost:4343/auth/session', {withCredentials: true})
+    // }
+
+    // static Logout() {
+    //     return axios.get('http://localhost:4343/auth/logout', {withCredentials: true})
+    // }    
 }
 
 export default UserDataService;
